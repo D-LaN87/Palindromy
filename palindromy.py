6bc434102990399ef096e7ebc27a524f902ef50a
Your Name <adamski_leszek@wp.pl>
2025-05-14 23:16:49 +0200

    Add description to plaindrome.py

--- palindromy.py
+++ palindromy.py
@@ -1,9 +1,20 @@
-
 def is_the_word_palindrome(word):
+
     if word == word[::-1]:
         return True
     else:
         return False
     
+"""Description of the definition
+
+    Args:
+        word: You put any word you like to check if its palindrome
+
+    if function word == word[::1] checks if word written backwards matches the original word (palindrome)
+      
+    Returns:
+        Boolean: Returns if the word is Palindrome or not (True or False)
+"""
+   
 print(is_the_word_palindrome("wojtek"))
 print(is_the_word_palindrome("anna"))
